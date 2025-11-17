86198d8de7467946c120f583520e16ea6c2d0b09
BACK-11 <back-11@Erxess-Mac-mini.local>
2025-11-17 20:49:58 +0800

    update rest

---
+++ src/controllers/bank.js
@@ -0,0 +1,25 @@
+import { BankService } from "../services/bank.js";
+
+export const checkBalanceController = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const aa = new BankService();
+
+    res.send(aa.checkBalance());
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+};import { BankService } from "../services/bank.js";
+
+export const checkBalanceController = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const aa = new BankService();
+
+    res.send(aa.checkBalance());
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+};
